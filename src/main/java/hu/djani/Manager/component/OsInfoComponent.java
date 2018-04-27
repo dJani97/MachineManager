01f1620ce7aa6357d8cdc5285fbf8633663de5c6
János Dobszai <dobszaijani@gmail.com>
2018-04-27 10:36:54 +0200

    osInfoClass; added a alert to user when not running as administrator

---
+++ src/main/java/hu/djani/Manager/component/OsInfoComponent.java
@@ -0,0 +1,38 @@
+package hu.djani.Manager.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OsInfoComponent {
+
+	public static final String USER_ROOT = "ROOT";
+	public static final String USER_NORMAL = "NORMAL";
+
+	public static final String OS_WINDOWS = "WINDOWS";
+	public static final String OS_UNIX = "UNIX";
+
+	public String getOsName() {
+		return System.getProperty("os.name").toUpperCase().contains(OS_WINDOWS) ? OS_WINDOWS : OS_UNIX;
+	}
+
+	public boolean isRunningAsAdmin() {
+		System.getProperties().list(System.out);
+		if (OS_UNIX == this.getOsName()) {
+			String user_name = System.getProperty("user.name");
+			if ("root".equals(user_name)) {
+				return true;
+			}
+		} else if (OS_WINDOWS == this.getOsName()) {
+			String user_name = System.getProperty("user.name");
+			System.out.println("__ USERNAME__:" + user_name);
+			if ("admin".equals(user_name)) {
+				return true;
+			}
+		} else {
+			return false;
+		}
+
+		return false;
+	}
+
+}
--- src/main/java/hu/djani/Manager/control/HomeController.java
+++ src/main/java/hu/djani/Manager/control/HomeController.java
@@ -1,14 +1,21 @@
 package hu.djani.Manager.control;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import hu.djani.Manager.component.OsInfoComponent;
+
 @Controller
 public class HomeController {
 
+	@Autowired
+	OsInfoComponent info;
+
 	@RequestMapping("/")
 	public String home(Model model) {
+		model.addAttribute("raisePermissionAlert", !info.isRunningAsAdmin());
 		return "index";
 	}
 
