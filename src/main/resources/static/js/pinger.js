dfe9cc3c80894213f9afb78f5e814d7c36f6baaa
János Dobszai <dobszaijani@gmail.com>
2018-05-02 22:16:47 +0200

    cleaning up JS and CSS

--- src/main/resources/static/js/pinger.js
+++ src/main/resources/static/js/pinger.js
@@ -1,35 +1,3 @@
-//function ping(ip, callback) {
-//    
-//    if (!this.inUse) {
-//        this.status = 'unchecked';
-//        this.inUse = true;
-//        this.callback = callback;
-//        this.ip = ip;
-//        var _that = this;
-//        this.img = new Image();
-//        this.img.onload = function () {
-//            _that.inUse = false;
-//            _that.callback('responded');
-//
-//        };
-//        this.img.onerror = function (e) {
-//            if (_that.inUse) {
-//                _that.inUse = false;
-//                _that.callback('responded', e);
-//            }
-//
-//        };
-//        this.start = new Date().getTime();
-//        this.img.src = "http://" + ip;
-//        this.timer = setTimeout(function () {
-//            if (_that.inUse) {
-//                _that.inUse = false;
-//                _that.callback('timeout');
-//            }
-//        }, 1500);
-//    }
-//};
-
 function ajax_ping(ip, callback) {
 	$.get("/api/ping/" + ip, response => {
 		if (response == true) {
@@ -63,7 +31,6 @@ function updateAllIcons() {
 	    })
 	})
 };
-
 
 window.onload = function() {
 	updateAllIcons();
